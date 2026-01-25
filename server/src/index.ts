95baa69e07a2fae3e65668219fc638b6208e2534
alemartel <martelmarrero@gmail.com>
2026-01-25 10:45:28 +0000

    use js extension

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,5 @@
 // Direct import so Vercel detects this as a Hono entrypoint (project uses "type": "module")
 import "hono";
-import app from "./api";
+import app from "./api.js";
 
 export default app;
